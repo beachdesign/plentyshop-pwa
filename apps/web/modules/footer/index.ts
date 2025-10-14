7c7eb3f3fdaa8497cfefaf91350f8d8b82e2b379
Christian Zehnter <zehnter@dortmund-beach.com>
2025-10-14 17:19:54 +0200

    dev

--- apps/web/modules/footer/index.ts
+++ apps/web/modules/footer/index.ts
@@ -2,15 +2,15 @@ import { createResolver, defineNuxtModule } from 'nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
-    name: 'BlocksFooter',
+    name: 'ScFooter',
   },
   setup (_options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     nuxt.hook('components:extend', (components) => {
-      const blocksFooter = components.find((c) => c.pascalName === 'BlocksFooter');
-      if (blocksFooter) {
-        blocksFooter.filePath = resolver.resolve('./BlocksFooter.vue');
+      const scFooter = components.find((c) => c.pascalName === 'BlocksFooter');
+      if (scFooter) {
+        scFooter.filePath = resolver.resolve('./Footer.vue');
       }
     });
   },
