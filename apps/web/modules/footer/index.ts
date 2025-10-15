9991e393642da884f7ba99a9c41fc331bb0c49f6
Christian Zehnter <zehnter@dortmund-beach.com>
2025-10-15 11:38:39 +0200

    Update index.ts

--- apps/web/modules/footer/index.ts
+++ apps/web/modules/footer/index.ts
@@ -8,10 +8,35 @@ export default defineNuxtModule({
     const resolver = createResolver(import.meta.url)
 
     nuxt.hook('components:extend', (components) => {
-      const scFooter = components.find((c) => c.pascalName === 'BlocksFooter');
+      const scFooter = components.find((c) => c.pascalName === 'FooterBlock');
       if (scFooter) {
         scFooter.filePath = resolver.resolve('./Footer.vue');
       }
     });
   },
 })
+
+// export default defineNuxtModule({
+//   meta: {
+//     name: 'ScFooter',
+//   },
+//   setup (_options, nuxt) {
+//     const resolver = createResolver(import.meta.url)
+
+//     nuxt.hook('components:extend', (components) => {
+//       // Entferne die originale Komponente
+//       const index = components.findIndex((c) => c.pascalName === 'BlocksFooter')
+//       if (index !== -1) {
+//         components.splice(index, 1)
+//       }
+
+//       // Füge die neue Komponente hinzu
+//       components.push({
+//         pascalName: 'BlocksFooter',
+//         filePath: resolver.resolve('./Footer.vue'),
+//         global: true,
+//         priority: 100
+//       })
+//     })
+//   }
+// })
